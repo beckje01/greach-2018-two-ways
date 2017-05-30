fd3d3d32e4171be57fcf145df389ed272ec84b45
Jeff Beck <beckje01@gmail.com>
2017-05-31 01:39:22 +0200

    Formatting

--- ratpack/src/ratpack/ratpack.groovy
+++ ratpack/src/ratpack/ratpack.groovy
@@ -4,15 +4,17 @@ import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
-  bindings {
-    module MarkupTemplateModule
-  }
+	bindings {
+		module MarkupTemplateModule
+	}
 
-  handlers {
-    get {
-      render groovyMarkupTemplate("index.gtpl", title: "My ratpack App")
-    }
+	handlers {
+		get {
+			render groovyMarkupTemplate("index.gtpl", title: "My ratpack App")
+		}
 
-    files { dir "public" }
-  }
+
+
+		files { dir "public" }      g
+	}
 }
